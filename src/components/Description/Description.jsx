18777865c888ffdbc3467af7a52634d2b1f3d7ba
DylanoHa <dylano.hartman@gmail.com>
2021-07-02 14:10:20 +0200

    Prettier

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,17 +1,17 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import styled from 'styled-components'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
-const DescriptionContainer = styled.div`
 
+const DescriptionContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 5vw;
 
-  .info{
+  .info {
     margin-left: 5vw;
     width: 40vw;
     display: flex;
@@ -21,36 +21,35 @@ const DescriptionContainer = styled.div`
 `
 
 const Description = ({ title, image, text, id }) => {
-  gsap.registerPlugin(ScrollTrigger);
-  const ref = useRef(null);
+  gsap.registerPlugin(ScrollTrigger)
+  const ref = useRef(null)
 
   useEffect(() => {
-    const element = ref.current;
-    console.log(element);
+    const element = ref.current
     gsap.fromTo(
-      element.querySelector(".title"+id),
+      element.querySelector('.title' + id),
       {
         opacity: 0,
-        y: -50
+        y: -50,
       },
       {
         opacity: 1,
         y: 0,
         scrollTrigger: {
           trigger: ref.current,
-          start: "top center+=100",
+          start: 'top center+=100',
           scrub: false,
-        }
+        },
       }
-    );  
-  }, []);
+    )
+  }, [])
 
   return (
     <DescriptionContainer ref={ref}>
       <img className={`img${id}`} src={image} alt='' />
 
-      <div className="info">
-        <h1 className={`title${id}`} >{title}</h1>
+      <div className='info'>
+        <h1 className={`title${id}`}>{title}</h1>
         <ReactMarkdown rehypePlugins={[rehypeRaw]} children={text} />
       </div>
     </DescriptionContainer>
